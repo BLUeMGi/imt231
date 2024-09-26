7fb9388206303fb0c34e996ec799f0b7875f073a
BLU eMGi <matilasalle0@gmail.com>
2024-09-26 02:24:13 -0400

    26/09/24_05 cambio a booleanos

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
 int main() {
-    int secreto = 42; // Número secreto del juego de Adivinanza
+    cout << "*****************************************" << endl;
+    cout << "* Bienvenido al Juego de la Adivinanza! *" << endl;
+    cout << "*****************************************" << endl;
+
+    const int NUMERO_SECRETO = 42;
     int adivina;
-    cout << "***************************************" << endl;
-    cout << "Bienvenido al Juego de Adivinanza!" << endl;
-    cout << "***************************************" << endl;
-    cout << "Adivina el número secreto: ";
+    bool acerto, mayor;
+
+    cout << "¿Cuál es el número? ";
     cin >> adivina;
 
-    if (adivina == secreto){
-        cout << "Felicidades!! Adivinó el número secreto" << endl;
+    acerto = adivina == NUMERO_SECRETO;
+    mayor = adivina > NUMERO_SECRETO;
+
+    cout << "El valor de su número es: " << adivina << endl;
+
+    if (acerto) {
+        cout << "¡Felicitaciones! Adivinaste el número secreto." << endl;
+    } else if (mayor) {
+        cout << "El número ingresado es mayor que el número secreto." << endl;
+    } else {
+        cout << "El número ingresado es menor que el número secreto." << endl;
     }
-    else if (adivina > secreto){
-        cout << "El número ingresado es mayor que el número secreto" << endl;
-    }
-    else {
-        cout << "El número ingresado es menor que el número secreto" << endl;
-    }
+
     return 0;
 }
+
 
 
 
