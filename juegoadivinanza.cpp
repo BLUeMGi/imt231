f98d76acfad42f796195bab331b92b80203f8c07
BLU eMGi <matilasalle0@gmail.com>
2024-09-26 02:03:14 -0400

    26/09/24_03 leer e imprimir valor

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -7,6 +7,12 @@ int main() {
     cout << "***********************************" << endl;
 
     int numero_secreto = 42;
-    cout << "El número secreto es ..." << numero_secreto << ". No lo diga a nadie!" << endl;
+    cout << "El número secreto es ..." << numero_secreto << ". No lo diga a para nadie!" << endl;
+
+    int adivina;
+    cout << "¿Cuál es el número? ";
+    cin >> adivina;
+    cout << "El valor de su número es: " << adivina << endl;
 }
+
 
